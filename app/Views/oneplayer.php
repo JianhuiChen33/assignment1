a14b08d1c495a6be4f547c02bf24e4422124e8cd
jianhui_chen <651215575@qq.com>
2020-10-27 21:40:20 +0800

    small testing change to news

--- app/Views/oneplayer.php
+++ app/Views/oneplayer.php
@@ -5,14 +5,15 @@
 	</head>
 	<body>
 		<h1>One  Paris Saint Germain star</h1>
-		<p><strong>ID:</strong> {id}</p>                
+		<p><strong>Number:</strong> {id}</p>                
                 <p><strong>Name:</strong> {name}</p>
-		<p><strong>height:</strong> {name}</p>
-                <p><strong>weight:</strong> {name}</p>
-                <p><strong>Arm spread:</strong> {name}</p>
-                <p><strong>Age:</strong> {name}</p>
-		<p><strong>Description:</strong> {description}</p>
+		<p><strong>height:</strong> {height}</p>
+                <p><strong>weight:</strong> {weight}</p>
+                <p><strong>Position:</strong> {Position}</p>
+                <p><strong>Age:</strong> {Age}</p>
+		<p><strong>Country：</strong> {Country}</p>
+                <p><strong>Played for the team：</strong> {Played for the team}</p>
                 <p>Pic:<img src="/image/{image}"/></p>
-		<p><a href="/home">Home</a></p>
+		<p><a href="/Paris">playerlist</a></p>
 	</body>
 </html>
